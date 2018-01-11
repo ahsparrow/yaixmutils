b8f4f761bb7434435a35beb810415f6ef669e893
Alan Sparrow <git@freeflight.org.uk>
2018-01-11 11:23:37 +0000

    More fiddling with dependency_links

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     ],
     keywords=['airspace', 'aixm', 'yaixm'],
     install_requires=["yaixm", "PyYAML", "pyparsing", "pygeodesy"],
-    dependency_links=["git+https://github.com/ahsparrow/yaixm.git"],
+    dependency_links=["git+https://github.com/ahsparrow/yaixm.git#egg=yaixm"],
     packages=find_packages(),
     entry_points={
         'console_scripts': [
