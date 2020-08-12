1c3a74b49bdb91bb80c1c624d8c52f345ee18605
Alan Sparrow <git@freeflight.org.uk>
2020-08-12 16:31:41 +0100

    Add missing cli scripts

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="yaixmutils",
-    version="1.1.0",
+    version="1.2.0",
     description="YAIXM utilities",
     url="https://gitlab.com/ahsparrow/yaixmutils",
     author="Alan Sparrow",
@@ -25,7 +25,10 @@ setup(
         'console_scripts': [
             "yaixm_release = yaixmutils.cli:release",
             "yaixm_convert_obstacle = yaixmutils.cli:convert_obstacle",
-            "yaixm_check_service = yaixmutils.cli:check_service"
+            "yaixm_check_service = yaixmutils.cli:check_service",
+            "yaixm_calc_ils = yaixmutils.cli:calc_ils",
+            "yaixm_calc_point = yaixmutils.cli:calc_point",
+            "yaixm_calc_stub = yaixmutils.cli:calc_stub"
         ]
     }
 )
